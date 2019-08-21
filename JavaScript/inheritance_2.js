7dc8aaac02e14a642e0d23d0cac5940fad2c34a0
Leonardo Eufrazio <leonardo.eufrazio@inatel.br>
2019-08-21 17:09:53 -0300

    Added breathe method and yell method

--- JavaScript/inheritance_2.js
+++ JavaScript/inheritance_2.js
@@ -2,24 +2,31 @@ let Homem = function (nome, idade) {
     this.nome = nome;
     this.idade = idade;
 }
+let gritar = function (nome) {
+    console.log(nome + " gritou");
+};
+let respirar = function (nome) {
+    console.log(nome + " respirou");
+};
 
 Homem.prototype.sexo = "masculino";
+Homem.prototype.gritar = gritar;
+Homem.prototype.respirar = respirar;
 
-/*let leo = new Homem('Leo', 22);
-console.log(leo.sexo);*/
+let leo = new Homem('Leo', 22);
+leo.gritar(leo.nome);
+leo.respirar(leo.nome);
 
-let _new = function (f) {
+/*let _new = function (f) {
     let obj = {};
     obj.__proto__ = f.prototype;
     f.apply(obj, Array.prototype.slice.call(arguments, 1));
     return obj;
-};
+};*/
 
-let pedro = _new(Homem, "Pedro", 22);
+//let pedro = _new(Homem, "Pedro", 22);
 
 /*let pedro = {};
 //Homem.call(pedro, "Pedro", 18);
 pedro.__proto__ = Homem.prototype;
 Homem.apply(pedro, ["Pedro", 18]);*/
-
-console.log(pedro.sexo);
